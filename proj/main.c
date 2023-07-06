6a2be6b8f52686672dacf2920d5b298abeb7ec3c
Matheus Home <matheusbaeck@gmail.com>
2023-07-06 02:26:55 +0200

    home ls working

--- proj/main.c
+++ proj/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
+/*   By: math42 <math42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 02:00:30 by mamagalh@st       #+#    #+#             */
-/*   Updated: 2023/07/05 21:43:15 by mamagalh@st      ###   ########.fr       */
+/*   Updated: 2023/07/06 02:24:42 by math42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,25 @@ int	get_std_paths(char **envp, char ***paths)
 int	get_command_pathname(char **comm, char **envp)
 {
 	char	**paths;
+	char	*path;
+	char	*temp;
 	int		i;
 
 	if (access(*comm, F_OK) == 0)
-		return (0);
-	if (!get_std_paths(envp, &paths))
-		return (-1);
+		return (printf("no add to path\n"),0);
+	if (get_std_paths(envp, &paths) == -1)
+		return (printf("no standart path\n")-1);
 	i = -1;
-	while (++i && paths[i])
+	while (paths[++i])
 	{
-		if (access(ft_strjoin(paths[i], *comm), F_OK) == 0)
-			return (0);
+		temp = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(temp, *comm);
+		free(temp);
+		printf("path::%s\n", path);
+		if (access(path, F_OK) == 0)
+			return (free(*comm), *comm = path, 0);
 	}
-	return (-1);
+	return (printf("command not found\n"), -1);
 }
 
 enum	e_main_errors {
@@ -60,8 +66,8 @@ typedef struct s_pipex_args
 {
 	int		fd_in;
 	int		fd_out;
-	char	*comm1;
-	char	*comm2;
+	char	*path1;
+	char	*path2;
 	char	**cmd1;
 	char	**cmd2;
 }			t_args;
@@ -71,22 +77,25 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_args	args;
 
-	printf("starts");
-	if (argc < 0)
-		return (-1);
+	if (argc < 5)
+		return (printf("few args\n"), -1);
 	args.fd_in = open(argv[1], O_RDONLY);
 	args.fd_out = open(argv[4], O_WRONLY);
 	if (!(args.fd_in && args.fd_out))
-		return (-2);
-	args.comm1 = ft_strtrim(argv[2], " ");
-	args.comm2 = ft_strtrim(argv[3], " ");
-	if (!(get_command_pathname(&args.comm1, envp)
-			&& get_command_pathname(&args.comm2, envp)))
-		return (free(args.comm1), free(args.comm2), -3);
+		return (printf("cant open files"), -2);
 	args.cmd1 = ft_split(argv[2], ' ');
 	args.cmd2 = ft_split(argv[3], ' ');
-	execve(args.comm1, args.cmd1, NULL);
-	return (free(args.comm1), free(args.comm2),
+	args.path1 = ft_strdup(args.cmd1[0]);
+	args.path2 = ft_strdup(args.cmd2[0]);
+	if ((get_command_pathname(&args.path1, envp)
+			|| get_command_pathname(&args.path2, envp)))
+		return (free(args.path1), free(args.path2), printf("command not found\n"), -3);
+	int id = fork();
+	if (id == 0)
+		execve(args.path1, args.cmd1, NULL);
+	else
+		execve(args.path2, args.cmd2, NULL);
+	return (free(args.path1), free(args.path2),
 		free(args.cmd1), free(args.cmd2), 0);
 }
 

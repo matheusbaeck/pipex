dd0bd51470907f56ff56c534eb3f934517e3258e
Matheus Home <matheusbaeck@gmail.com>
2023-09-14 23:01:47 +0200

    home

--- test/a.sh
+++
@@ -1,8 +0,0 @@
-PIPEPATH=/Users/mamagalh/pipex/proj/pipex
-
-echo ""
-echo "runing test 1.1, wrong command, error expected"
-rm -fr f_outfile*
-$PIPEPATH   f_infile "greep -v .c" "wrong" f_outfile
-< f_infile greep -v .c | wrong > f_outfile2
-diff f_outfile f_outfile2 -s
